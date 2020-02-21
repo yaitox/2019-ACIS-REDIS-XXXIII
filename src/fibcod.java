6adecb362742aafe9bc203a8c31a364157ee6bc2
angelasofiaremolinagutierrez <angelasofiaremolinagutierrez@gmail.com>
2020-02-21 15:53:33 -0500

    kitchen

--- src/fibcod.java
+++ src/fibcod.java
@@ -29,8 +29,8 @@ public class fibcod {
                 double den = Math.pow(5,0.5); //raiz de 5
                 double num = (Math.pow(1.618034,(suma.length()+1)-i))-(Math.pow(-0.618034,(suma.length()+1)-i));
                 double fib = num/den;
-                //sumaA = sumaA + (f* Double.parseDouble(String.valueOf(suma.charAt(suma.length()-i))));
                 sumaA = sumaA + (fib*Character.getNumericValue(suma.charAt(i)));
+                //revisar el ultimo input ¿por que da cero?
             }
             System.out.println(Math.round(sumaA)%524288);
         }
--- src/kitchen.java
+++ src/kitchen.java
@@ -1,32 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class kitchen {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-        int trays = Integer.parseInt(sc.nextLine());
 
-        while(sc.nextLine()!= null) {
-            List<Integer> widths = new ArrayList<Integer>();
-            List<Integer> lengths = new ArrayList<Integer>();
-            for (int i = 0; i < trays; i++) {
-                String[] wl = sc.nextLine().split(" ");
-                String w = wl[0];
-                int width = Integer.parseInt(w);
+        BufferedReader br = new BufferedReader ( new InputStreamReader(System.in));
+        Scanner input = new Scanner(System.in);
+        String m;
+        List<Integer> answer = new ArrayList<>();
+        while(!(m=br.readLine()).equals("")) {
+            int cont=1;
+            List<Integer> res = new ArrayList<>();
+            List<Integer> dX=new ArrayList<>();
+            List<Integer> dY=new ArrayList<>();
 
-                String l = wl[1];
-                int length = Integer.parseInt(l);
-                widths.add(width);
-                lengths.add(length);
+            int n=Integer.parseInt(m);
+
+            for(int i = 0; i< n; i++) {
+                int x=input.nextInt();
+                int y=input.nextInt();
+
+
+
+                res.add(x*10+y);
+            }
+            Collections.sort(res);
+            Collections.reverse(res);
+
+
+            for(int i=0;i<res.size();i++) {
+                dX.add(res.get(i)/10);
+                dY.add(res.get(i)%10);
             }
 
-            int wMax = Collections.max(widths);
-            int lMax = Collections.max(lengths);
+            for(int k = 0; k < n-1; k ++) {
 
-            //https://www.jstor.org/stable/2691523?seq=1
-            //https://www.quora.com/Computational-Geometry-Can-one-rectangle-fit-inside-the-other
-            //https://math.stackexchange.com/questions/847282/fitting-rectangle-inside-another-rectangle-in-diagonal
+
+
+
+                if(((dX.get(k)>=dX.get(k+1))||(dX.get(k)>=dY.get(k+1)))&&((dY.get(k)>=dX.get(k+1))||(dY.get(k)>=dY.get(k+1)))){
+                    continue;
+                }
+
+                cont++;
+
+            }
+            answer.add(cont);
+
+
 
         }
+        for(int answe : answer) {
+            System.out.println(answe);
+        }
+
     }
 }
