5ad10cd2aaa60233fd6b9ed2286006b88a7408cd
angelasofiaremolinagutierrez <angelasofiaremolinagutierrez@gmail.com>
2020-02-23 21:47:32 -0500

    correccion bracelets

--- src/Solucionados/bracelets.java
+++ src/Solucionados/bracelets.java
@@ -1,27 +1,32 @@
 package Solucionados;
 //funcionaaaaa :D
-//solo le falta la lectura de several cases
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class bracelets {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int lenBrace = Integer.parseInt(scan.nextLine());
-        String brace1 = scan.nextLine();
-        String brace2 = scan.nextLine();
-        int cont = lenBrace;
-        for (int i = 0; i <lenBrace ; i++) {
-            String last = brace2.substring(lenBrace-1); //coge el ultimo caracter
-            String rest = brace2.substring(0,lenBrace-1); //coge del primero al penultimo caracter
-            String newBrace2 = last+rest;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while (!(line=br.readLine()).equals(""))
+        {
+            int lenBrace = Integer.parseInt(line);
+            String brace1 = br.readLine();
+            String brace2 = br.readLine();
+            int cont = lenBrace;
+            for (int i = 0; i <lenBrace ; i++) {
+                String last = brace2.substring(lenBrace-1); //coge el ultimo caracter
+                String rest = brace2.substring(0,lenBrace-1); //coge del primero al penultimo caracter
+                String newBrace2 = last+rest;
 
-            for (int j = 0; j < lenBrace; j++) {
+                for (int j = 0; j < lenBrace; j++) {
 
-                if((newBrace2.charAt(j)=='R')&&(newBrace2.charAt(j) == brace1.charAt(j))){
-                    cont = cont-1;
+                    if((newBrace2.charAt(j)=='R')&&(newBrace2.charAt(j) == brace1.charAt(j))){
+                        cont = cont-1;
+                    }
                 }
+                brace2 = newBrace2;
             }
-            brace2 = newBrace2;
+            System.out.println(cont);
         }
-        System.out.println(cont);
     }
 }
